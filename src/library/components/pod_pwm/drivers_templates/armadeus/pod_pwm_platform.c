b21743f4d07947354b3f02abc335a01a47f7cdf0
Kevin JOLY <joly.kevin25@gmail.com>
2012-09-04 18:06:43 +0200

    Add pod_pwm component
    Signed-off-by: Fabien Marteau <fabien.marteau@armadeus.com>

---
+++ src/library/components/pod_pwm/drivers_templates/armadeus/pod_pwm_platform.c
@@ -0,0 +1,81 @@
+/*
+ * Linux kernel platform informations for the POD component pod_pwm
+ *
+ * (C) Copyright 2012 - EIRBOT <eirbot.enseirb.fr>
+ * Author: Kevin JOLY joly.kevin25@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <mach/fpga.h>
+
+#include "../../virtual_components/pod_pwm/pod_pwm.h"
+
+/*$foreach:instance$*/
+#define /*$instance_name$*/_BASE /*$registers_base_address:swb16$*/
+/*$foreach:instance:end$*/
+
+/*$foreach:instance$*/
+static struct resource /*$instance_name$*/_resources[] = {
+	[0] = {
+		.start = ARMADEUS_FPGA_BASE_ADDR + /*$instance_name$*/_BASE,
+		.end = ARMADEUS_FPGA_BASE_ADDR + /*$instance_name$*/_BASE + 0x04,
+		.flags = IORESOURCE_MEM | IORESOURCE_MEM_16BIT,
+	}
+};
+
+void /*$instance_name$*/_release(struct device *dev)
+{
+	dev_dbg(dev, "released\n");
+}
+
+static struct platform_device /*$instance_name$*/_pdev = {
+	.name = DRIVER_NAME,
+	.id = /*$generic:id$*/,
+	.dev = {
+		.release = /*$instance_name$*/_release,
+	},
+	.num_resources = ARRAY_SIZE(/*$instance_name$*/_resources),
+	.resource = /*$instance_name$*/_resources,
+};
+/*$foreach:instance:end$*/
+
+static int __init pod_pwm_platform_init(void)
+{
+	int ret;
+
+/*$foreach:instance$*/
+	ret = platform_device_register(&/*$instance_name$*/_pdev);
+	if (ret < 0)
+		printk(KERN_ERR "Error: Can't init device pod_pwm\n");
+/*$foreach:instance:end$*/
+
+		return ret;
+}
+module_init(pod_pwm_platform_init);
+
+static void __exit pod_pwm_platform_exit(void)
+{
+/*$foreach:instance$*/
+	platform_device_unregister(&/*$instance_name$*/_pdev);
+/*$foreach:instance:end$*/
+}
+module_exit(pod_pwm_platform_exit);
+
+MODULE_AUTHOR("Kevin Joly <joly.kevin25@gmail.com>");
+MODULE_DESCRIPTION("Platform driver for pod_pwm driver");
+MODULE_LICENSE("GPL v2");
---
+++ src/library/components/pod_pwm/hdl/pod_pwm.vhd
@@ -0,0 +1,101 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pod_pwm is
+    generic (
+    	id : natural := 1;
+        wb_size : natural := 16
+    );
+	port(
+		gls_reset : in std_logic;
+		gls_clk : in std_logic;
+		wbs_add : in std_logic_vector(1 downto 0);
+		wbs_writedata : in std_logic_vector(wb_size-1 downto 0);
+		wbs_readdata : out std_logic_vector(wb_size-1 downto 0);
+		wbs_strobe : in std_logic;
+		wbs_cycle : in std_logic;
+		wbs_write : in std_logic;
+		wbs_ack : out std_logic;
+		pwm_output : out std_logic
+	);
+end pod_pwm;
+
+architecture Behavioral of pod_pwm is
+	type state_wb_type is (init_wb, write_register, read_register);
+	signal state_wb : state_wb_type := init_wb;
+	signal t_on : std_logic_vector(wb_size-1 downto 0);
+	signal div : std_logic_vector(wb_size-1 downto 0);
+begin
+
+	wb_interface : process(gls_clk, gls_reset)
+	begin
+		if gls_reset = '1' then
+			state_wb <= init_wb;
+			t_on <= (others => '0');
+			div <= (others => '0');
+		elsif rising_edge(gls_clk) then
+			case state_wb is
+				when init_wb =>
+					if (wbs_strobe and wbs_write) = '1' then
+						state_wb <= write_register;
+					elsif (wbs_strobe = '1') and (wbs_write = '0') then
+						state_wb <= read_register;
+					end if;
+				when write_register =>
+					if wbs_strobe = '0' then
+						state_wb <= init_wb;
+					else
+						if wbs_add = "01" then
+							t_on <= wbs_writedata(wb_size-1 downto 0);
+						elsif wbs_add = "10" then
+							div <= wbs_writedata(wb_size-1 downto 0);
+						end if;
+					end if;
+				when read_register =>
+					if wbs_strobe = '0' then
+						state_wb <= init_wb;
+					end if;
+				when others =>
+					state_wb <= init_wb;
+			end case;	
+		end if;
+	end process;
+	
+	pwm_generation : process(gls_clk)
+		variable cpt : integer range 0 to 65535 := 0;
+	begin
+		if rising_edge(gls_clk) then
+
+			if cpt < to_integer(unsigned(div))-1 then
+				if cpt < to_integer(unsigned(t_on)) then
+					pwm_output <= '1';
+				else
+					pwm_output <= '0';
+				end if;
+				
+				cpt := cpt + 1;
+			else
+				pwm_output <= '0';
+				cpt := 0;
+			end if;
+		end if;
+	end process;
+		
+  	wbs_readdata(wb_size-1 downto 0) <= 	std_logic_vector(to_unsigned(id, wb_size))    when wbs_add = "00" else
+														t_on  when wbs_add = "01" else
+														div   when wbs_add = "10" else
+														(others => '0');
+
+
+end Behavioral;
