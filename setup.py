050862d26e486698f42f36cd2a8d2e21b3043e9a
Fabien Marteau <fabien.marteau@armadeus.com>
2014-02-27 15:12:31 +0100

    [setup.py] setup.py is now strict according to pep8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------------------------------
 # Name:     setup.py
-# Purpose:  
+# Purpose:
 # Author:   Fabien Marteau <fabien.marteau@armadeus.com>
 # Created:  16/02/2009
 #-----------------------------------------------------------------------------
@@ -12,14 +12,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
 #-----------------------------------------------------------------------------
@@ -30,17 +30,19 @@
 #-----------------------------------------------------------------------------
 
 from distutils.core import setup
-import os,re
+import os
+import re
 import sys
 sys.path.append("src/bin/")
 from version import *
 
-def visit(libfile,dirname,names):
+
+def visit(libfile, dirname, names):
     """ function used for getLibraryTree to walk throw library tree"""
     for file in names:
-        filepath = os.path.join(dirname,file)
+        filepath = os.path.join(dirname, file)
         if not os.path.isdir(filepath):
-            if not re.search(r".svn",filepath):
+            if not re.search(r".svn", filepath):
                 # FIXME:
                 # I can't find how to split with os.path !
                 # will be used when package_data work
@@ -48,10 +50,11 @@ def visit(libfile,dirname,names):
                 #libfile.append(realpath)
                 libfile.append(filepath)
 
+
 def getTree(directory):
     """ return a tuple list of files """
     libfile = []
-    os.path.walk(os.path.join("src",directory),visit,libfile)
+    os.path.walk(os.path.join("src", directory), visit, libfile)
     new_libfile = []
     for path_file in libfile:
         new_libfile.append('/'.join(path_file.split('/')[1:]))
@@ -68,26 +71,27 @@ package_files_list.extend(getTree("busses"))
 package_files_list.extend(getTree("toolchains"))
 package_files_list.extend(getTree("tests"))
 
-datafiles=[ ('bin',['src/bin/pod']) ]
+datafiles = [('bin', ['src/bin/pod'])]
 
-setup(  name='PeriphOnDemand',
-        version=getVersion(),
-        url='https://sourceforge.net/projects/periphondemand',
-        author='Fabien Marteau and Nicolas Colombain',
-        author_email='<fabien.marteau@armadeus.com>,<nicolas.colombain@armadeus.com>,',
-        maintainer='Fabien Marteau',
-        maintainer_email='fabien.marteau@armadeus.com',
-        package_dir = {"periphondemand":"src"},
-        packages=['periphondemand',
-                  'periphondemand.bin',
-                  'periphondemand.bin.code',
-                  'periphondemand.bin.code.vhdl',
-                  'periphondemand.bin.commandline',
-                  'periphondemand.bin.core',
-                  'periphondemand.bin.toolchain',
-                  'periphondemand.bin.utils',
-                  ],
-        package_data = {'periphondemand':package_files_list},
-        data_files=datafiles,
-        license='GPL',
+setup(name='PeriphOnDemand',
+      version=getVersion(),
+      url='https://sourceforge.net/projects/periphondemand',
+      author='Fabien Marteau and Nicolas Colombain',
+      author_email='<fabien.marteau@armadeus.com>,"' +
+                   '<nicolas.colombain@armadeus.com>',
+      maintainer='Fabien Marteau',
+      maintainer_email='fabien.marteau@armadeus.com',
+      package_dir={"periphondemand": "src"},
+      packages=['periphondemand',
+                'periphondemand.bin',
+                'periphondemand.bin.code',
+                'periphondemand.bin.code.vhdl',
+                'periphondemand.bin.commandline',
+                'periphondemand.bin.core',
+                'periphondemand.bin.toolchain',
+                'periphondemand.bin.utils',
+                ],
+      package_data={'periphondemand': package_files_list},
+      data_files=datafiles,
+      license='GPL',
 )
