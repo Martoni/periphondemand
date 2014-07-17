fd2cb1154c1ed41d6227f3948e9f66c057aea67c
Fabien Marteau <fabien.marteau@armadeus.com>
2014-07-17 18:17:38 +0200

    functionals_tests: adding structure for functionals tests

---
+++ functionals_tests/test_launcher.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Author:   Fabien Marteau <fabien.marteau@armadeus.com>
+# Created:  17/07/2014
+#-----------------------------------------------------------------------------
+#  Copyright (2014)  Armadeus Systems
+#-----------------------------------------------------------------------------
+""" class test_launcher
+"""
+
+import sys
+import xmlrunner
+import unittest
+import os
+
+from periphondemand.bin import pod 
+
+class test_launcher(unittest.TestCase):
+    """
+    """
+
+    def test_apf27_uarts_gpios(self):
+        """ testing apf27_uarts_gpios script"""
+        with self.assertRaises(SystemExit):
+            pod.main(['pod', '-s', 'functionals_tests/apf27_uarts_gpios.pod'])
+        self.assertTrue(os.path.isfile("apf27uartgpio/objs/top_apf27uartgpio.bit"))
+        os.system("rm -rf  apf27uartgpio")
+
+
+if __name__ == "__main__":
+    print "Functionnals tests launcher"
+    unittest.main(
+            testRunner=xmlrunner.XMLTestRunner(
+                output='test-reports'))
--- runtests.sh
+++ runtests.sh
@@ -3,7 +3,8 @@ rm -rf htmlcov
 rm -rf .coverage
 rm -rf coverage.xml
 ln -s src periphondemand
-export PYTHONPATH=$PYTHONPATH:./
+export PYTHONPATH=./:$PYTHONPATH
 python-coverage run --source periphondemand --branch units_tests/test_project.py
+python-coverage run -a --source periphondemand --branch functionals_tests/test_launcher.py
 python -m coverage xml
 python -m coverage html
