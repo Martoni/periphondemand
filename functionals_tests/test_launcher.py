4106a1f4c431e05c9ba1ef76e6d82435173aa455
Fabien Marteau <fabien.marteau@armadeus.com>
2014-07-18 16:27:23 +0200

    functionals_tests: adding test for irq ext

--- functionals_tests/test_launcher.py
+++ functionals_tests/test_launcher.py
@@ -27,6 +27,15 @@ class test_launcher(unittest.TestCase):
         self.assertTrue(os.path.isfile("apf27uartgpio/objs/top_apf27uartgpio.bit"))
         os.system("rm -rf  apf27uartgpio")
 
+    def test_apf27_i2cledbuttonextint(self):
+        """ testing apf27_uarts_gpios script"""
+        with self.assertRaises(SystemExit):
+            pod.main(['pod',
+                      '-s',
+                      'functionals_tests/apf27_i2cledbuttonextint.pod'])
+        self.assertTrue(os.path.isfile("i2cledbuttonextint/objs/top_i2cledbuttonextint.bit"))
+        os.system("rm -rf  i2cledbuttonextint")
+
 
 if __name__ == "__main__":
     print "Functionnals tests launcher"
