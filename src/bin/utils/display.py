42b99e00299aece0a40aa5704465e6bf10015de2
Fabien Marteau <fabien.marteau@armadeus.com>
2014-07-21 15:51:20 +0200

    src/bin/utils/display.py: now strict according to pep8

--- src/bin/utils/display.py
+++ src/bin/utils/display.py
@@ -28,16 +28,13 @@
 # Date       By        Changes
 #
 #-----------------------------------------------------------------------------
-
-__doc__ = ""
-__version__ = "1.0.0"
-__versionTime__ = "18/07/2008"
-__author__ = "Fabien Marteau <fabien.marteau@armadeus.com>"
+""" Managing printing messages """
 
 from periphondemand.bin.define import *
 from periphondemand.bin.utils.settings import Settings
 
 settings = Settings()
+
 
 class Display(object):
     """ Class used to display messages,
@@ -52,53 +49,56 @@ class Display(object):
         def __init__(self):
             self.msglist = []
             self.val = None
+
         def __str__(self):
             out = ""
             for msg in self.msglist:
-                if   msg[1] == 0:
-                     try:
-                         if settings.color()==1:
-                             out = out + COLOR_ERROR+"[ERROR]"+COLOR_END+"  : "+\
-                                     COLOR_ERROR_MESSAGE+msg[0].strip()+\
-                                     COLOR_END+"\n"
-                         else:
-                             out = out+"[ERROR]  : "+msg[0].strip()+"\n"
-                     except NameError:
-                         out = out+"[ERROR]  : "+msg[0].strip()+"\n"
+                if msg[1] == 0:
+                    try:
+                        if settings.color() == 1:
+                            out = out + COLOR_ERROR + "[ERROR]" +\
+                                    COLOR_END + "  : " +\
+                                    COLOR_ERROR_MESSAGE + msg[0].strip() +\
+                                    COLOR_END + "\n"
+                        else:
+                            out = out + "[ERROR]  : " + msg[0].strip() + "\n"
+                    except NameError:
+                        out = out + "[ERROR]  : " + msg[0].strip() + "\n"
                 elif msg[1] == 1:
-                     try:
-                         if settings.color()==1:
-                             out = out + COLOR_WARNING+"[WARNING]"+COLOR_END+": "+\
-                                     COLOR_WARNING_MESSAGE+msg[0].strip()+\
-                                     COLOR_END+"\n"
-                         else:
-                             out = out+"[WARNING]: "+msg[0].strip()+"\n"
-                     except NameError:
-                         out = out+"[WARNING]: "+msg[0].strip()+"\n"
+                    try:
+                        if settings.color() == 1:
+                            out = out + COLOR_WARNING + "[WARNING]" +\
+                                    COLOR_END + ": " +\
+                                    COLOR_WARNING_MESSAGE + msg[0].strip() +\
+                                    COLOR_END + "\n"
+                        else:
+                            out = out + "[WARNING]: " + msg[0].strip() + "\n"
+                    except NameError:
+                        out = out + "[WARNING]: " + msg[0].strip() + "\n"
                 elif msg[1] == 2:
-                     try:
-                         if settings.color()==1:
-                             out = out + COLOR_INFO+"[INFO]"+COLOR_END+" : "+\
-                                     COLOR_INFO_MESSAGE+msg[0].strip()+\
-                                     COLOR_END+"\n"
-                         else:
-                             out = out+"[INFO] : "+msg[0].strip()+"\n"
-                     except NameError:
-                         out = out+"[INFO] : "+msg[0].strip()+"\n"
-                else :
-                     out = out+msg[0].strip()+"\n"
+                    try:
+                        if settings.color() == 1:
+                            out = out + COLOR_INFO + "[INFO]" +\
+                                    COLOR_END + " : " +\
+                                    COLOR_INFO_MESSAGE + msg[0].strip() +\
+                                    COLOR_END + "\n"
+                        else:
+                            out = out + "[INFO] : " + msg[0].strip() + "\n"
+                    except NameError:
+                        out = out + "[INFO] : " + msg[0].strip() + "\n"
+                else:
+                    out = out + msg[0].strip() + "\n"
 
             self.msglist = []
             return out
 
-        def msg(self,msg,level=3):
-            self.msglist.append((msg,level))
+        def msg(self, msg, level=3):
+            self.msglist.append((msg, level))
 
-    def __new__(c): # _new_ is always class method
+    def __new__(c):  # _new_ is always class method
         if not Display.instance:
-               Display.instance = Display.__Display()
+                Display.instance = Display.__Display()
         return Display.instance
 
     def __getattr__(self, attr):
-       return getattr(self.instance, attr)
-
+        return getattr(self.instance, attr)
