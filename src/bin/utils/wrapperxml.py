ba57e6594731156e7da4ea126163ee99fe788396
Fabien Marteau <fabien.marteau@armadeus.com>
2015-05-06 15:10:06 +0200

    src/bin/utils/wrapperxml.py: now strict according to pep8

--- src/bin/utils/wrapperxml.py
+++ src/bin/utils/wrapperxml.py
@@ -14,18 +14,15 @@
 # Date       By        Changes
 #
 # ----------------------------------------------------------------------------
-
-__doc__ = ""
-__version__ = "1.0.0"
-__versionTime__ = "23/04/2008"
-__author__ = "Fabien Marteau <fabien.marteau@armadeus.com>"
+""" main xml object """
 
 import os
 import xml.etree.cElementTree as ET
 
 from periphondemand.bin.utils.error import Error
 
-def XMLBeautifier(xml_data):
+
+def xml_beautifier(xml_data):
     """
     This function make XML output looks better and more readable.
     Fabrice Mousset <fabrice.mousset@laposte.net>
@@ -48,13 +45,14 @@ def XMLBeautifier(xml_data):
                     xml_ident += 4
     return xml_text
 
+
 class WrapperXml(object):
     """Simple class manage XML
         attributes:
             tree -- root tree xml component
     """
 
-    def __init__(self,**args):
+    def __init__(self, **args):
         """ init function,
             __init__(self,node)   # parameter is wrapperXml node
             __init__(self,etnode) # parameter is ElementTree node
@@ -73,32 +71,44 @@ class WrapperXml(object):
         elif "file" in args:
             self.__initfile(args["file"])
         else:
-            raise Error("Keys unknown in WrapperXml",0)
+            raise Error("Keys unknown in WrapperXml", 0)
 
-    def __initfile(self,filename):
+    def __initfile(self, filename):
+        """ initialize with filename"""
         try:
             self.openXml(filename)
-        except IOError,e:
-            raise Error(str(e),0)
-    def __initnode(self,node):
+        except IOError, error:
+            raise Error(str(error), 0)
+
+    def __initnode(self, node):
+        """ initilize with node """
         self.tree = node.tree
-    def __initetnode(self,etnode):
+
+    def __initetnode(self, etnode):
+        """ initialize with etnode """
         self.tree = etnode
-    def __initnodename(self,nodename):
+
+    def __initnodename(self, nodename):
+        """ initialize with nodename """
         self.tree = ET.Element(nodename)
-    def __initnodestring(self,nodestring):
+
+    def __initnodestring(self, nodestring):
+        """ initialize with nodestring """
         try:
             self.tree = ET.fromstring(nodestring)
-        except SyntaxError,e:
-              raise Error("XML malformed :\n"+str(e),0)
+        except SyntaxError, error:
+            raise Error("XML malformed :\n" + str(error), 0)
 
     def __str__(self):
-        return ('<?xml version="1.0" encoding="utf-8"?>\n' +\
-                XMLBeautifier(ET.tostring(self.tree,"utf-8")))
+        return ('<?xml version="1.0" encoding="utf-8"?>\n' +
+                xml_beautifier(ET.tostring(self.tree, "utf-8")))
 
     def getParent(self):
+        """ get parent node """
         return self.parent
+
     def setParent(self, parent):
+        """ set parent node """
         self.parent = parent
 
     def getSubNodeList(self, nodename, subnodename):
@@ -115,7 +125,7 @@ class WrapperXml(object):
     def getNodeList(self, nodename):
         """ Return a list of nodes
         """
-        nodelist =[]
+        nodelist = []
         for node in self.tree.findall(nodename):
             tmp = WrapperXml(etnode=node)
             nodelist.append(tmp)
@@ -137,7 +147,7 @@ class WrapperXml(object):
         """
         if "nodename" in keys:
             node = self.getNode(nodename=keys["nodename"])
-            if node==None:
+            if node is None:
                 node = self.addNode(nodename=keys["nodename"])
             if "subnode" in keys:
                 return node.addNode(node=keys["subnode"])
@@ -151,13 +161,13 @@ class WrapperXml(object):
                     return node.addNode(nodename=subnodename,
                                         attributedict=keys["attributedict"])
                 else:
-                    raise Error("Key not known in addSubNode "+str(keys),0)
+                    raise Error("Key not known in addSubNode " + str(keys), 0)
             else:
-                raise Error("Key not known in addSubNode"+str(keys),0)
+                raise Error("Key not known in addSubNode" + str(keys), 0)
         else:
-            raise Error("Key not known in addSubNode"+str(keys),0)
+            raise Error("Key not known in addSubNode" + str(keys), 0)
 
-    def addNode(self,**keys):
+    def addNode(self, **keys):
         """ Add a node in the tree,
             addNode(self,node)
             addNode(self,nodename)
@@ -167,19 +177,19 @@ class WrapperXml(object):
         if "node" in keys:
             node = keys["node"]
         elif "nodename" in keys:
-            node =WrapperXml(nodename=keys["nodename"])
+            node = WrapperXml(nodename=keys["nodename"])
             if "attributename" in keys:
-                node.setAttribute(keys["attributename"],keys["value"])
+                node.setAttribute(keys["attributename"], keys["value"])
             elif "attributedict" in keys:
                 attributedict = keys["attributedict"]
                 for key in attributedict:
-                    node.setAttribute(key,attributedict[key])
+                    node.setAttribute(key, attributedict[key])
         else:
-            raise Error("Keys not known in addNode",0)
+            raise Error("Keys not known in addNode", 0)
 
         try:
             self.tree.append(node.tree)
-        except AttributeError: # if tree doesn't exits
+        except AttributeError:  # if tree doesn't exits
             self.tree = node.tree
         return node
 
@@ -187,57 +197,60 @@ class WrapperXml(object):
         """ delete a node, attribute can be single or, if they're multiple
             attribute, attribute is a dictionnary
             delNode(self, node, attribute=None, value=None)
-            delNode(self, node_name, {attribute1:value1, attribute1:value1, ...})
+            delNode(self, node_name, {attribute1:value1, attribute1:value1,..})
         """
         if type(node) == str:
             nodename = node
             for element in self.getNodeList(nodename):
                 if(element.tree.tag == nodename):
-                    if(attribute==None):
+                    if attribute is None:
                         self.tree.remove(element.tree)
-                    elif (type(attribute) == str) and (element.getAttributeValue(attribute) == value):
+                    elif (type(attribute) == str) and\
+                            (element.getAttributeValue(attribute) == value):
                         self.tree.remove(element.tree)
                     elif type(attribute) == dict:
                         match = 1
                         for key in attribute:
-                            if element.getAttributeValue(key) != attribute[key]:
+                            if element.getAttributeValue(key) !=\
+                                    attribute[key]:
                                 match = 0
-                        if match==1:
+                        if match == 1:
                             self.tree.remove(element.tree)
         else:
             self.tree.remove(node.tree)
 
-    def delSubNode(self,nodename,subnodename,attribute=None,value=None):
+    def delSubNode(self, nodename, subnodename, attribute=None, value=None):
         node = self.getNode(nodename)
-        node.delNode(subnodename,attribute,value)
+        node.delNode(subnodename, attribute, value)
 
-    def getAttributeValue(self,key,subnodename=None):
-        if subnodename == None:
+    def getAttributeValue(self, key, subnodename=None):
+        if subnodename is None:
             return self.tree.get(key)
         else:
             node = self.tree.find(subnodename)
-            if node == None:
-                raise Error("No tag named "+str(subnodename))
+            if node is None:
+                raise Error("No tag named " + str(subnodename))
             return node.get(key)
-        raise Error("No attribute named "+key+" for tag "+str(subnodename))
+        raise Error("No attribute named " + key +
+                    " for tag " + str(subnodename))
 
     def getAttributeNameList(self, subnodename=None):
-        if subnodename == None:
+        if subnodename is None:
             return self.tree.keys()
         else:
             node = self.tree.find(subnodename)
-            if node == None:
-                raise Error("No tag named "+str(subnodename))
+            if node is None:
+                raise Error("No tag named " + str(subnodename))
             return node.keys()
         raise Error("getAttributeNameList error")
 
-    def setAttribute(self,key,value,subname=None):
-        if subname == None:
+    def setAttribute(self, key, value, subname=None):
+        if subname is None:
             self.tree.attrib[key] = value
             return value
         else:
-            if self.tree.find(subname) == None:
-                ET.SubElement(self.tree,subname)
+            if self.tree.find(subname) is None:
+                ET.SubElement(self.tree, subname)
             self.tree.find(subname).attrib[key] = value
             return value
 
@@ -247,55 +260,64 @@ class WrapperXml(object):
         except AttributeError:
             return ""
 
-    def setDescription(self,description):
+    def setDescription(self, description):
         if self.tree.find("description") is not None:
             self.tree.find("description").text = description
         else:
-            desc = ET.SubElement(self.tree,"description")
+            desc = ET.SubElement(self.tree, "description")
             desc.text = description
         return description
 
     def getSize(self):
         return self.getAttributeValue("size")
 
-    def setSize(self,size):
-        self.setAttribute("size",str(size))
+    def setSize(self, size):
+        self.setAttribute("size", str(size))
+
     def getName(self):
         return self.getAttributeValue("name")
-    def setName(self,value):
-        return self.setAttribute("name",value)
-    def getVersion(self):
-        return self.getAttributeValue("version")
-    def setVersion(self,version):
-        return self.setAttribute("version",version)
 
-    def openXml(self,filename,string=None):
+    def setName(self, value):
+        return self.setAttribute("name", value)
+
+    def getVersion(self):
+        """ get the version """
+        return self.getAttributeValue("version")
+
+    def setVersion(self, version):
+        """ set the version """
+        return self.setAttribute("version", version)
+
+    def openXml(self, filename, string=None):
         """ open xml, parameters can be filename or xml string
         """
-        if type(filename)==str and string!=None:
-              try:
-                  self.tree = ET.fromstring(filename)
-              except SyntaxError,e:
-                  raise Error("Xml malformed in "+filename+" : \n"+str(e),0)
+        if type(filename) == str and string is not None:
+            try:
+                self.tree = ET.fromstring(filename)
+            except SyntaxError, error:
+                raise Error("Xml malformed in " +
+                            filename + " : \n" + str(error), 0)
         else:
-           try:
-                xmlfile = open(filename,'r')
-           except IOError,e:
-                raise Error(str(e),0)
-           content = xmlfile.read().replace(r'<?xml version="1.0" encoding="utf-8"?>','')
-           try:
-               self.tree = ET.fromstring(content)
-           except SyntaxError,e:
-                raise Error("Xml malformed in "+filename+" :\n"+str(e),0)
+            try:
+                xmlfile = open(filename, 'r')
+            except IOError, error:
+                raise Error(str(error), 0)
+            content =\
+                xmlfile.read().replace(
+                    r'<?xml version="1.0" encoding="utf-8"?>', '')
+            try:
+                self.tree = ET.fromstring(content)
+            except SyntaxError, error:
+                raise Error("Xml malformed in " +
+                            filename + " :\n" + str(error), 0)
 
-    def createXml(self,tag):
+    def createXml(self, tag):
         self.tree = ET.Element(tag)
 
-    def saveXml(self,pathname):
-        f = open(pathname,"w")
+    def saveXml(self, pathname):
+        f = open(pathname, "w")
         f.write(str(self))
         f.close()
 
     def isVoid(self):
         return self.void
-
