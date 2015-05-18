43f3beec33251c695d72cf54dd41920315a7547f
Fabien Marteau <fabien.marteau@armadeus.com>
2015-05-18 15:12:23 +0200

    src/bin/core/component.py: better code quality

--- src/bin/core/component.py
+++ src/bin/core/component.py
@@ -51,14 +51,16 @@ class Component(WrapperXml):
             __init__(self,parent)
         """
         self.void = void
-        # Project that use the component
-        self.parent = parent
+
         self.interfaceslist = []
         self.genericslist = []
         self.hdl_fileslist = []
         self.driver_templateslist = []
         self.interruptslist = []
         self.constraintslist = []
+
+        # Project that use the component
+        self.parent = parent
 
     def loadNewInstance(self, libraryname, componentname,
                         componentversion, instancename):
@@ -70,31 +72,30 @@ class Component(WrapperXml):
             raise PodError("Instance name can't be " +
                            "the same name as projectname", 0)
         # test if component exist
-        if not sy.fileExist(
-                project.library.getLibraryPath(libraryname) +
-                                "/" + componentname):
+        if not sy.fileExist(project.library.getLibraryPath(libraryname) +
+                            "/" + componentname):
             raise PodError("No component with name " +
                            libraryname + "." + componentname, 0)
 
-        #test if several componentversion
+        # test if several componentversion
         if componentversion is None:
-            if len(project.get_components_versions(
-                            libraryname, componentname)) > 1:
+            if len(project.get_components_versions(libraryname,
+                                                   componentname)) > 1:
                 raise PodError("Component version must be chosen :" +
                                str(project.get_components_versions(
                                    libraryname, componentname)), 0)
             else:
                 try:
                     componentversion = project.get_components_versions(
-                            libraryname, componentname)[0]
+                        libraryname, componentname)[0]
                 except IndexError:
                     raise PodError("No xml description of component", 0)
         if instancename is None:
             instancename =\
-                    componentname + "%02d" %\
-                    len(project.get_instances_list_of_component(componentname))
+                componentname + "%02d" %\
+                len(project.get_instances_list_of_component(componentname))
 
-        #copy and rename directory
+        # copy and rename directory
         sy.copyDirectory(project.library.getLibraryPath(libraryname) +
                          "/" + componentname,
                          SETTINGS.projectpath + COMPONENTSPATH)
@@ -105,16 +106,16 @@ class Component(WrapperXml):
                                "/" + instancename)
         except PodError:  # if directory exist
             pass
-        #Rename xml file
+        # Rename xml file
         sy.renameFile(SETTINGS.projectpath +
-                COMPONENTSPATH + "/" + instancename +
-                "/" + componentversion + XMLEXT,
-                SETTINGS.projectpath + COMPONENTSPATH +
-                "/" + instancename + "/" + instancename + XMLEXT)
+                      COMPONENTSPATH + "/" + instancename +
+                      "/" + componentversion + XMLEXT,
+                      SETTINGS.projectpath + COMPONENTSPATH +
+                      "/" + instancename + "/" + instancename + XMLEXT)
 
-        #load component
+        # load component
         self.loadInstance(instancename)
-        #Connect platform connection
+        # Connect platform connection
         self.autoconnectPin()
 
     def loadInstance(self, instancename):
@@ -122,8 +123,8 @@ class Component(WrapperXml):
         """
         # load xml file
         WrapperXml.__init__(self, file=SETTINGS.projectpath +
-                                       COMPONENTSPATH + "/" + instancename +
-                                       "/" + instancename + XMLEXT)
+                            COMPONENTSPATH + "/" + instancename +
+                            "/" + instancename + XMLEXT)
 
         # Fill objects list
         if self.getNode("interfaces") is not None:
@@ -142,14 +143,14 @@ class Component(WrapperXml):
             for element in\
                     self.getSubNodeList("driver_files", "driver_templates"):
                 self.driver_templateslist.append(
-                        Driver_Templates(self, node=element))
+                    Driver_Templates(self, node=element))
 
         if self.getNode("interrupts") is not None:
             for element in self.getSubNodeList("interrupts", "interrupt"):
                 self.interruptslist.append(
-                        self.getInterface(
-                            element.getAttributeValue("interface")).getPort(
-                                element.getAttributeValue("port")))
+                    self.getInterface(
+                        element.getAttributeValue("interface")).getPort(
+                            element.getAttributeValue("port")))
 
         if self.getNode("constraints") is not None:
             for element in self.getSubNodeList("constraints", "constraint"):
@@ -158,6 +159,7 @@ class Component(WrapperXml):
         self.setInstanceName(instancename)
 
     def getConstraintsList(self):
+        """ Get list of constraints """
         return self.constraintslist
 
     def autoconnectPin(self):
@@ -167,21 +169,25 @@ class Component(WrapperXml):
             interface.autoconnectPin()
 
     def getHdl_filesList(self):
+        """ Get list of HDL files """
         return self.hdl_fileslist
 
     def getHDLTop(self):
+        """ Get hdl top file """
         for hdlfile in self.getHdl_filesList():
             if hdlfile.isTop():
                 return hdlfile
         return None
 
     def getHDLFile(self, filename):
+        """ get hdl file """
         for hdlfile in self.getHdl_filesList():
             if hdlfile.getFileName() == filename:
                 return hdlfile
         raise PodError("no hdl file named " + filename)
 
     def addHdl_file(self, hdl_file):
+        """ add hdl file """
         self.addSubNode(nodename="hdl_files", subnode=hdl_file)
         return self.hdl_fileslist.append(hdl_file)
 
@@ -192,12 +198,15 @@ class Component(WrapperXml):
         return os.path.join(librarypath, self.getName())
 
     def getInterruptList(self):
+        """ Get interrupt list """
         return self.interruptslist
 
     def getGenericsList(self):
+        """ Get generics parameters list """
         return self.genericslist
 
     def getFPGAGenericsList(self):
+        """ Get fpga specifics generic list """
         fpgalist = []
         for generic in self.getGenericsList():
             if generic.getDestination() == "fpga" or\
@@ -206,17 +215,20 @@ class Component(WrapperXml):
         return fpgalist
 
     def getGeneric(self, genericname):
+        """ get a generic """
         for generic in self.getGenericsList():
             if generic.getName() == genericname:
                 return generic
         raise PodError("No generic with name " + genericname, 0)
 
     def addGeneric(self, generic):
+        """ Adding a generic """
         generic.parent = self
         self.genericslist.append(generic)
         self.addSubNode(nodename="generics", subnode=generic)
 
     def getInterface(self, interfacename):
+        """ Get an interface by name """
         for interface in self.interfaceslist:
             if interface.getName() == interfacename:
                 return interface
@@ -224,8 +236,7 @@ class Component(WrapperXml):
                        " does not exists", 0)
 
     def getMasterInterfaceList(self):
-        """ return a list of master interface
-        """
+        """ return a list of master interface """
         interfacelist = []
         for interface in self.getInterfacesList():
             if interface.getClass() == "master":
@@ -233,8 +244,7 @@ class Component(WrapperXml):
         return interfacelist
 
     def getSlaveInterfaceList(self):
-        """ return a list of slave interface
-        """
+        """ return a list of slave interface """
         interfacelist = []
         for interface in self.getInterfacesList():
             if interface.getClass() == "slave":
@@ -242,14 +252,14 @@ class Component(WrapperXml):
         return interfacelist
 
     def getSysconInterface(self):
+        """ Get the syscon interface"""
         for interface in self.getInterfacesList():
             if interface.getClass() == "clk_rst":
                 break
         return interface
 
     def createInterface(self, interfacename):
-        """ Create an interface and add it in component
-        """
+        """ Create an interface and add it in component """
         if interfacename in [interface.getName() for
                              interface in self.getInterfacesList()]:
             raise PodError("Interface " + interfacename +
@@ -258,27 +268,28 @@ class Component(WrapperXml):
         self.addInterface(interface)
 
     def addInterface(self, interface):
-        """ Add an interface in component
-        """
+        """ Add an interface in component """
         interface.parent = self
         self.interfaceslist.append(interface)
         self.addSubNode(nodename="interfaces", subnode=interface)
 
     def getInterfacesList(self):
+        """ Get the list of interfaces """
         return self.interfaceslist
 
     def getDriver_TemplateList(self):
+        """ get the driver template list """
         return self.driver_templateslist
 
     def getDriver_Template(self, architecture):
+        """ Get a driver template """
         for driverT in self.getDriver_TemplateList():
             if driverT.getArchitecture() == architecture:
                 return driverT
         return None
 
     def saveInstance(self):
-        """ Save component in project directory files
-        """
+        """ Save component in project directory files """
         if not sy.dirExist(SETTINGS.projectpath + COMPONENTSPATH +
                            "/" + self.getInstanceName()):
             sy.makeDirectory(SETTINGS.projectpath + COMPONENTSPATH +
@@ -288,29 +299,29 @@ class Component(WrapperXml):
                      self.getInstanceName() + ".xml")
 
     def delInstance(self):
-        """ suppress component instance
-        """
+        """ suppress component instance """
         if not self.isPlatform():
             sy.delDirectory(SETTINGS.projectpath + COMPONENTSPATH + "/" +
                             self.getInstanceName())
 
     def getInstanceName(self):
+        """ get the name of this component instance"""
         return self.getAttributeValue("instance_name")
 
     def setInstanceName(self, instancename):
+        """ set the name of this instance """
         return self.setAttribute("instance_name", instancename)
 
     def setNum(self, num):
-        """ select the instance number
-        """
+        """ select the instance number """
         return self.setAttribute("num", str(num))
 
     def getNum(self):
+        """ Get the instance number """
         return self.getAttributeValue("num")
 
     def invertDir(self, dirname):
-        """ invert direction given in params
-        """
+        """ invert direction given in params """
         if dirname == "in":
             return "out"
         if dirname == "out":
@@ -338,8 +349,7 @@ class Component(WrapperXml):
                                 pinsource)
 
     def connect_bus(self, interfacemaster, instanceslave, interfaceslave):
-        """ Connect an interface bus master to slave
-        """
+        """ Connect an interface bus master to slave """
         interface = self.getInterface(interfacemaster)
         if interface.getName() is None:
             raise PodError(interfacemaster + " is not a bus", 1)
@@ -347,8 +357,7 @@ class Component(WrapperXml):
 
     def del_bus(self, instanceslavename, interfacemaster=None,
                 interfaceslavename=None):
-        """ Delete bus connection that refer to instanceslavename
-        """
+        """ Delete bus connection that refer to instanceslavename """
         if interfacemaster is None:
             for interface in self.getInterfacesList():
                 try:
@@ -360,9 +369,8 @@ class Component(WrapperXml):
             interface.del_bus(instanceslavename, interfaceslavename)
 
     def connectClkDomain(self, instancedestname, interfacesourcename,
-                               interfacedestname):
-        """ Connect clock domain
-        """
+                         interfacedestname):
+        """ Connect clock domain """
         interface = self.getInterface(interfacesourcename)
         interface.connectClkDomain(instancedestname, interfacedestname)
 
@@ -374,7 +382,7 @@ class Component(WrapperXml):
             raise PodError("No HDL top file in component " +
                            str(self.getName()))
         portlist = hdltop.ports
-        if not portname in [port.getName() for port in portlist]:
+        if portname not in [port.getName() for port in portlist]:
             raise PodError("Port named " + portname + " can't be found in " +
                            hdltop.getFileName())
 
@@ -382,7 +390,7 @@ class Component(WrapperXml):
         isinfreelist, interface_old = self.portIsInFreeList(portname)
         if not isinfreelist:
             raise PodError("Port named " + portname +
-                        " is already placed in " + interface_old)
+                           " is already placed in " + interface_old)
         # take interface
         interface = self.getInterface(interfacename)
         # create port object
@@ -441,14 +449,13 @@ class Component(WrapperXml):
         return display_port
 
     def isPlatform(self):
+        """ is this component instance is a Platform ?"""
         return False
 
-    ##################################
     # Settings attributes for nodes
     def setGeneric(self, generic_name, attribute_name,
-                         attribute_value):
-        """Add or modify attribute value for a node
-        """
+                   attribute_value):
+        """Add or modify attribute value for a node """
         generic = self.getGeneric(generic_name)
         if attribute_name == "name":
             generic.setName(attribute_value)
@@ -466,7 +473,7 @@ class Component(WrapperXml):
             raise PodError("Unknown attribute " + str(attribute_name))
 
     def setHDL(self, file_name, attribute_name,
-            attribute_value):
+               attribute_value):
         """ Setting HDL files """
         HDL = self.getHDLFile(file_name)
         if attribute_name == "filename":
@@ -484,9 +491,8 @@ class Component(WrapperXml):
             raise PodError("Unknown attribute " + str(attribute_name))
 
     def setInterface(self, interface_name, attribute_name,
-                           attribute_value):
-        """Add or modify attribute value for a node
-        """
+                     attribute_value):
+        """Add or modify attribute value for a node """
         interface = self.getInterface(interface_name)
         if attribute_name == "name":
             interface.setName(attribute_value)
@@ -500,7 +506,7 @@ class Component(WrapperXml):
             raise PodError("Unknown attribute " + str(attribute_name))
 
     def setPort(self, interface_name, port_name,
-                      attribute_name, attribute_value):
+                attribute_name, attribute_value):
         """ Setting port"""
         interface = self.getInterface(interface_name)
         port = interface.getPort(port_name)
@@ -516,7 +522,7 @@ class Component(WrapperXml):
             raise PodError("Attribute " + str(attribute_name) + " unknown")
 
     def setRegister(self, interface_name, register_name,
-                          attribute_name, attribute_value):
+                    attribute_name, attribute_value):
         """ Setting register """
         interface = self.getInterface(interface_name)
         register = interface.getRegister(register_name)
