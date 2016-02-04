6347818e4e26ad76682d409c3e18768655eac2df
Fabien Marteau <fabien.marteau@armadeus.com>
2016-02-04 11:15:25 +0100

    functionnal_tests: project name bug in apf6_wbextvalidation, change syntax for all tests

--- functionals_tests/test_launcher.py
+++ functionals_tests/test_launcher.py
@@ -23,29 +23,32 @@ class test_launcher(unittest.TestCase):
 
     def test_apf27_i2cledbuttonextint(self):
         """ testing apf27_uarts_gpios script"""
-        os.system("rm -rf  i2cledbuttonextint")
+        PROJECT_NAME = "i2cledbuttonextint"
+        os.system("rm -rf  " + PROJECT_NAME)
         with self.assertRaises(SystemExit):
             pod.main(['pod',
                       '-s',
                       'functionals_tests/apf27_i2cledbuttonextint.pod'])
-        self.assertTrue(os.path.isfile("i2cledbuttonextint/objs/top_i2cledbuttonextint.bit"))
+        self.assertTrue(os.path.isfile(PROJECT_NAME + "/objs/top_i2cledbuttonextint.bit"))
         os.system("rm -rf  i2cledbuttonextint")
 
     def test_apf27_uarts_gpios(self):
         """ testing apf27_uarts_gpios script"""
-        os.system("rm -rf  apf27uartgpio")
+        PROJECT_NAME = "apf27uartgpio"
+        os.system("rm -rf  " + PROJECT_NAME)
         with self.assertRaises(SystemExit):
             pod.main(['pod', '-s', 'functionals_tests/apf27_uarts_gpios.pod'])
-        self.assertTrue(os.path.isfile("apf27uartgpio/objs/top_apf27uartgpio.bit"))
+        self.assertTrue(os.path.isfile(PROJECT_NAME + "/objs/top_apf27uartgpio.bit"))
         os.system("rm -rf  apf27uartgpio")
 
     def test_apf6_wbextvalidation(self):
         """ testing apf6_wbextvaldation script"""
-        os.system("rm -rf  apf6_validation")
+        PROJECT_NAME = "apf6_wbextvalidation"
+        os.system("rm -rf  " + PROJECT_NAME)
         with self.assertRaises(SystemExit):
             pod.main(['pod', '-s', 'functionals_tests/apf6_wbextvalidation.pod'])
-        self.assertTrue(os.path.isfile("apf6_wbextvalidation/binaries/top_test_apf6_wbextvalidation.core.rbf"))
-        self.assertTrue(os.path.isfile("apf6_wbextvalidation/binaries/top_test_apf6_wbextvalidation.periph.rbf"))
+        self.assertTrue(os.path.isfile(PROJECT_NAME + "/binaries/top_test_apf6_wbextvalidation.core.rbf"))
+        self.assertTrue(os.path.isfile(PROJECT_NAME + "/binaries/top_test_apf6_wbextvalidation.periph.rbf"))
         os.system("rm -rf  apf6_validation")
 
 
